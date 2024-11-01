d1ba7b705a7b0278211c4c0e2f56022831663660
ZeroX-DG <viethungax@gmail.com>
2024-11-02 08:35:41 +1300

    use node 10 because bug

--- themes/thehappyloneguy/scripts/video.js
+++ themes/thehappyloneguy/scripts/video.js
@@ -1,7 +1,7 @@
 hexo.extend.tag.register(
   "video",
   function (args) {
-    const width = args[1] ?? "100%";
+    const width = args[1] || "100%";
     return `<video width="${width}" controls> <source src="${args[0]}" type="video/mp4"> </video>`;
   },
   { async: true },
